e98623696e6976f5c5b47e62ae7fea4b1217863b
UlrikeWerner <ulrike_werner@web.de>
2023-09-20 12:12:25 +0200

    add standard Exception handling and add GlobalExceptionHandlerTest

---
+++ src/test/java/de/neuefische/springexceptionhandlingtask/GlobalExceptionHandlerTest.java
@@ -0,0 +1,31 @@
+package de.neuefische.springexceptionhandlingtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.CoreMatchers.containsString;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class GlobalExceptionHandlerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @DirtiesContext
+    public void testHandleUnexpectedError() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/cars"))
+                .andExpect(status().isInternalServerError())
+                .andExpect(jsonPath("$.errorMessage").isNotEmpty())
+                .andExpect(jsonPath("$.errorMessage").value(containsString("An unexpected error: ")));
+    }
+}
